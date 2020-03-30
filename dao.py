44774584c79c73b52e4540db8fca89d5766c7b3b
xiaofang <1149160879@qq.com>
2020-03-30 16:25:05 +0800

    通过成绩计算课程之间的关联关系

--- dao.py
+++ dao.py
@@ -21,57 +21,10 @@ def getStuIdAndScoreByCourseId(course_ids):
     return res
 
 
-def compare(param, expected):
-    if param is None or expected is None:
-        raise Exception(print("成绩为空或者期望成绩为空"))
-    # 两个长度相等
-    if len(param) is len(expected):
-        return param <= expected
-    # 长度不同
-    else:
-        if param[0] is expected[0]:
-            return param > expected
-        else:
-            return param < expected
-
-
-def getStudentFiveGrade(expected):
-    """五级课程"""
-    course_ids = getCoursesByScoreMethod(1)
-    grade_five = getStuIdAndScoreByCourseId(course_ids)
-    grade_dict = {}
-    for item in grade_five:
-        # 成绩大于计算分析的期望值 五级制比如为B A
-        if compare(item["grade"], expected):
-            key = item["stuId"]
-            grade = item["grade"]
-            if item["stuId"] not in grade_dict:
-                value = [grade]
-                grade_dict[key] = value
-            else:
-                grade_dict[key].append(grade)
-    grade_list = []
-    for (k, v) in grade_dict.items():
-        grade_list.append(v)
-    return grade_list
-
-
-def getStudentHundredGrade(expected):
-    """百分课程"""
-    course_ids = getCoursesByScoreMethod(3)
-    grade_hun = getStuIdAndScoreByCourseId(course_ids)
-    grade_dict = {}
-    for item in grade_hun:
-        # 成绩大于计算分析的期望值 五级制比如为B A
-        if item["grade"] >= expected:
-            key = item["stuId"]
-            grade = item["grade"]
-            if item["stuId"] not in grade_dict:
-                value = [grade]
-                grade_dict[key] = value
-            else:
-                grade_dict[key].append(grade)
-    grade_list = []
-    for (k, v) in grade_dict.items():
-        grade_list.append(v)
-    return grade_list
+# 通过课程id获取课程信息
+def getCourseById(course_id):
+    db = DBHelper()
+    sql = "select *" \
+          "from course where id = %s" % course_id
+    res = db.select(sql)
+    return res[0]
--- service.py
+++ service.py
@@ -1,13 +1,88 @@
-from app.analysis.generateRule import apriori
+from app.analysis.generateRule import *
 from app.dao import *
 
 
-def getStudentClassGrades():
-    # 五级值成绩
-    five_grade = getStudentFiveGrade("B")
-    apriori(five_grade)
+def getMaxRelated(rules):
+    max_rules = []
+    max = 0
+    for i in range(len(rules)):
+        item = rules[i]
+        if item[2] >= max:
+            max = item[2]
+    for item in rules:
+        if item[2] is max:
+            item[0] = list(item[0])
+            item[1] = list(item[1])
+            max_rules.append(item)
+    return max_rules
 
-    # 百分成绩
-    hun_grade = getStudentHundredGrade("80")
-    apriori(hun_grade)
 
+def convert(grade):
+    if grade is "A":
+        return 100
+    if grade is "A-":
+        return 89
+    if grade is "B+":
+        return 84
+    if grade is "B":
+        return 80
+    if grade is "B-":
+        return 77
+    if grade is "C+":
+        return 74
+    if grade is "C":
+        return 71
+    if grade is "C-":
+        return 67
+    if grade is "D+":
+        return 64
+    if grade is "D":
+        return 62
+    if grade is "F":
+        return 59
+    raise BaseException(print("成绩错误"))
+
+
+def getStudentGrade(expected):
+    """五级课程"""
+    course_ids = getCoursesByScoreMethod(1)
+    grade_five = getStuIdAndScoreByCourseId(course_ids)
+    for item in grade_five:
+        item["grade"] = convert(item["grade"])
+
+    """百分课程"""
+    course_ids = getCoursesByScoreMethod()
+    grade_hun = getStuIdAndScoreByCourseId(course_ids)
+    grades = grade_hun+grade_five
+    grade_dict = {}
+    for item in grades:
+        # 成绩大于计算分析的期望值 五级制比如为B A
+        if item["grade"] >= expected:
+            key = item["stuId"]
+            grade = item["grade"]
+            if item["stuId"] not in grade_dict:
+                value = [grade]
+                grade_dict[key] = value
+            else:
+                grade_dict[key].append(grade)
+    grade_list = []
+    for (k, v) in grade_dict.items():
+        grade_list.append(v)
+    return grade_list
+
+
+# 返回五级制中课程存在的关联关系和百分制课程关联关系
+def getStudentClassGradesRelatedRules():
+    grade = getStudentGrade("80")
+    L, suppData = apriori(grade)
+    rules = generateRules(L, suppData, minConf=0.5)
+    max_rules = getMaxRelated(rules)
+    course_rules = []
+    for item in max_rules:
+        course_rule_item = (
+            getCourseById(item[0]),
+            getCourseById(item[1]),
+            item[2]
+        )
+        course_rules.append(course_rule_item)
+    return course_rules
